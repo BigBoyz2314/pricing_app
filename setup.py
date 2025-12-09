41e7b62a439a7686aff0f57fbf800d208d5f41f3
BigBoyz2314 <zazimran@gmail.com>
2025-12-09 13:54:12 +0500

    Update setup.py to include author details, email, URL, license, and additional metadata for package distribution

--- setup.py
+++ setup.py
@@ -9,10 +9,22 @@ setuptools.setup(
     description="Pricing Calculator for Frappe/ERPNext",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    author="",
+    author="Pricing Calculator Team",
+    author_email="support@example.com",
+    url="https://example.com/pricing-calculator",
+    license="MIT",
     packages=setuptools.find_packages(),
     include_package_data=True,
     zip_safe=False,
+    install_requires=["frappe"],
+    python_requires=">=3.10",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Framework :: Frappe",
+        "Operating System :: OS Independent",
+    ],
 )
 
 
